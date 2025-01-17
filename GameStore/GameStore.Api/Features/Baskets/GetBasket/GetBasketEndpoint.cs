4614c71463debbedbb5d7a1eed966f6fcdeaf5a6
B.N. Berrevoets <bert@berrevoets.net>
2025-01-17 23:39:44 +0100

    Using claims-based authorization
    Add auth requirement and refactor MapGetBasket method
    
    Refactored the MapGetBasket method in GetBasketEndpoint.cs for better readability and maintainability. Added an authorization requirement to ensure that only users with the "basket.read" claim can access the endpoint. Improved code structure and indentation for clarity.

--- GameStore/GameStore.Api/Features/Baskets/GetBasket/GetBasketEndpoint.cs
+++ GameStore/GameStore.Api/Features/Baskets/GetBasket/GetBasketEndpoint.cs
@@ -9,24 +9,25 @@ public static class GetBasketEndpoint
     public static void MapGetBasket(this IEndpointRouteBuilder app)
     {
         app.MapGet("/{userId:guid}", async (Guid userId, GameStoreContext dbContext) =>
-        {
-            if (userId == Guid.Empty) return Results.BadRequest();
+            {
+                if (userId == Guid.Empty) return Results.BadRequest();
 
-            var basket = await dbContext.Baskets
-                .Include(basket => basket.Items)
-                .ThenInclude(item => item.Game)
-                .FirstOrDefaultAsync(basket => basket.Id == userId) ?? new CustomerBasket { Id = userId };
+                var basket = await dbContext.Baskets
+                    .Include(basket => basket.Items)
+                    .ThenInclude(item => item.Game)
+                    .FirstOrDefaultAsync(basket => basket.Id == userId) ?? new CustomerBasket { Id = userId };
 
-            var dto = new BasketDto(
-                basket.Id,
-                basket.Items.Select(item => new BasketItemDto(
-                    item.GameId,
-                    item.Game!.Name,
-                    item.Game!.Price,
-                    item.Quantity,
-                    item.Game!.ImageUri)).OrderBy(item => item.Name));
+                var dto = new BasketDto(
+                    basket.Id,
+                    basket.Items.Select(item => new BasketItemDto(
+                        item.GameId,
+                        item.Game!.Name,
+                        item.Game!.Price,
+                        item.Quantity,
+                        item.Game!.ImageUri)).OrderBy(item => item.Name));
 
-            return Results.Ok(dto);
-        });
+                return Results.Ok(dto);
+            })
+            .RequireAuthorization(builder => builder.RequireClaim("scope", "basket.read"));
     }
 }
